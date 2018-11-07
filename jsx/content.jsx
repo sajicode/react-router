862dc4276f8c2e1af7c266a202c6bf1bbca4a8ed
sajicode <sajioloye@gmail.com>
2018-11-07 22:43:15 +0100

    set up content file for use by other files

---
+++ jsx/content.jsx
@@ -0,0 +1,32 @@
+const React = require('react');
+const { Link } = require('react-router');
+
+class Content extends React.Component {
+	render() {
+		return (
+			<div>
+				<h1>Rumi's Poetry</h1>
+				<div className="navbar navbar-dark">
+					<ul className="nav nav-pills navbar-nav">
+						<li className={this.context.router.isActive('/about') ? 'active' : ''}>
+							<Link to="/about" activeClassName="active">
+								About
+							</Link>
+						</li>
+						<li className={this.context.router.isActive('/poems') ? 'active' : ''}>
+							<Link to="/poems" activeClassName="active">
+								Poetry
+							</Link>
+						</li>
+						<li className={this.context.router.isActive('/contact') ? 'active' : ''}>
+							<Link to="/contact" activeClassName="active">
+								Contact Us
+							</Link>
+						</li>
+					</ul>
+				</div>
+				{this.props.children}
+			</div>
+		);
+	}
+}
