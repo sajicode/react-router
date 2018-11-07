b07929c420cf57a586e7230226070e5b3243e652
sajicode <sajioloye@gmail.com>
2018-11-07 22:13:51 +0100

    populate poem.js file

--- poems.js
+++ poems.js
@@ -1,1 +1,66 @@
-module.exports = [];
+module.exports = [
+	{
+		id: 'confused',
+		title: 'Confused and Disraught',
+		link: 'http://www.rumi.org.uk/mystical_poems.htm#ConfusedAndDistraught',
+		text:
+			'Again I am raging, I am in such a state by your soul that every bond you bind, I break, by your soul. I am like heaven, like the moon, like a candle by your glow; I am all reason, all love, all soul, by your soul. My joy is of your doing, my hangover of your thorn; whatever side you turn your face, I turn mine, by your soul. I spoke in error; it is not surprising to speak in error in this state, for this moment I cannot tell cup from wine, by your soul. I am that madman in bonds who binds the "divs"; I, the madman, am a Solomon with the "divs", by your soul.',
+		img: './img/unsplash.jpg'
+	},
+	{
+		id: 'new-rule',
+		title: 'A New Rule',
+		link: 'http://www.rumi.org.uk/mystical_poems.htm#ANewRule',
+		text:
+			'It is the rule with drunkards to fall upon each other, to quarrel, become violent, and make a scene. The lover is even worse than a drunkard. I will tell you what love is: to enter a mine of gold. And what is that gold? The lover is a king above all kings, unafraid of death, not at all interested in a golden crown. The dervish has a pearl concealed under his patched cloak. Why should he go begging door to door?',
+		img: './img/unsplash.jpg'
+	},
+	{
+		id: 'drunkards',
+		title: 'It is the Rule with Drunkards',
+		link: 'http://www.rumi.org.uk/mystical_poems.htm#ItIsTheRuleWithDrunkards',
+		text:
+			'I, heart-forlorn and heart-yielded, fallen upon your way - by Allah, I know of no other place to fall. If I broke your bowl, I am drunk, my idol.I am drunk - leave me not from you hand to fall into danger. This is a newborn rule, a newly enacted decree - to shatter  glasses, and to fall upon the glassmaker!',
+		img: './img/unsplash.jpg'
+	},
+	{
+		id: 'ode',
+		title: 'Ode 2180',
+		link: 'http://www.rumi.org.uk/mystical_poems.htm#Ode2180',
+		text:
+			'From these depths depart towards heaven;may your soul be happy, journey joyfully. You have escaped from the city full of fear and trembling; happily become a resident of the Abode of Security. If the body’s image has gone, await the image-maker; if the body is utterly ruined, become all soul.',
+		img: './img/unsplash.jpg'
+	},
+	{
+		id: 'love',
+		title: 'Ode 2180',
+		link: 'http://www.rumi.org.uk/mystical_poems.htm#Thisislove',
+		text:
+			'This is love: to fly to heaven, every moment to rend a hundred veils; At first instance, to break away from breath-- first step to renounce feet; To disregard this world, to see only that which you yourself have seen. Bird, speak the tongue of birds: I can heed your cipher!',
+		img: './img/unsplash.jpg'
+	},
+	{
+		id: 'sweet',
+		title: 'Sweetly Parading',
+		link: 'http://www.rumi.org.uk/mystical_poems.htm#Sweetlyparading',
+		text:
+			'Sweetly parading you go my soul of soul, go not without me; life of your friends, enter not the garden without me. Sky, revolve not without me; moon, shine not without me; earth travel not without me, and time, go not without me. With you this world is joyous, and with you that world is joyous; in this world dwell not without me, and to that world depart not without me.',
+		img: './img/unsplash.jpg'
+	},
+	{
+		id: 'reason',
+		title: 'Reason Says',
+		link: 'http://www.rumi.org.uk/mystical_poems.htm#ReasonSays',
+		text:
+			'Reason says, "I will beguile him with the tongue;" Love says, "Be silent. I will beguile him with the soul." The soul says to the heart, "Go, do not laugh at me and yourself.What is there that is not his, that I may beguile him thereby ?" He is not sorrowful and anxious and seeking oblivion that I may beguile him with wine and a heavy measure.',
+		img: './img/unsplash.jpg'
+	},
+	{
+		id: 'i-have-come',
+		title: 'I Have Come',
+		link: 'http://www.rumi.org.uk/mystical_poems.htm#IHaveComeSoThat',
+		text:
+			'I have come so that, tugging your ear, I may draw you to me, unheart and unself you, plant you in my heart and soul. Rosebush, I have come a sweet springtide unto you, to seize you very gently in my embrace and squeeze you. I have come to adorn you in this worldly abode, to convey you above the skies like lovers prayers.',
+		img: './img/unsplash.jpg'
+	}
+];
